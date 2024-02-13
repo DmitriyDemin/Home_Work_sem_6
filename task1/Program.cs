d1ac959fae77fcf450e52d0dad0ee71ae22fe368
Dmitriy_Demin <d.demin@inbox.ru>
2024-02-13 20:06:52 +0400

    completion_task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+using System;
+namespace MatricesAndStrings
+{
+    class Task1
+    {
+        public static void Main()
+        {
+            Console.Clear();
+            Console.Write("введите количесво строк в массиве:   ");
+            int x = int.Parse(Console.ReadLine()!);
+            Console.Write("введите количесво столбцов в массиве:   ");
+            int y = int.Parse(Console.ReadLine()!);
+            char[,] symbols = new char[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.WriteLine("введите символ для заполнения массива:  ");
+                    symbols[i, j] = char.Parse(Console.ReadLine()!);
+                }
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write($"{symbols[i, j]} \t");
+
+                }
+                Console.WriteLine();
+            }
+            string result = "";
+            foreach (char element in symbols)
+            {
+                result += element;
+            }
+            Console.WriteLine(result);
+
+        }
+
+
+    }
+}
