8ca77d2a1b7dbb1875c0c33867d4f76f086f8b83
Dmitriy_Demin <d.demin@inbox.ru>
2024-02-14 19:58:15 +0400

    completed_task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку,
+//  в которой все заглавные буквы заменены на строчные.
+
+using System;
+namespace MatricesAndStrings
+{
+    class Task2
+    {
+        public static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("введите строку содержащую латинские буквы в обоих регистрах:   ");
+            string set = Console.ReadLine()!;
+            Console.WriteLine($"строка после ввода: {set}");
+            char[] chars = new char[set.Length];
+            for (int i = 0; i < set.Length; i++)
+            {
+                chars[i] = char.ToLower(set[i]);
+            }
+            Console.WriteLine($"строка после преобразования: {string.Join("", chars)}");
+        }
+    }
+}
+            
+
+
